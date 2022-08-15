4561a15ac2824b7edc8005368d898ab59c6a37dc
Stanley Lim <slim@Stanleys-MacBook-Air.local>
2022-08-15 13:20:04 -0400

    Incorrect Page Implemented

--- src/components/Correct.js
+++ src/components/Correct.js
@@ -1,8 +1,13 @@
+import audioIconGreen from '../images/audio-icon-green.png'
+
 const Correct = ({guess, handleGuess}) => {
     return (
         <>
-            <div className='good-message'>Correct! How do you feel?</div>
-            <div className='good-answer'>Answer:<br />{guess}</div>
+            <div className='good-message'>Correct! How Do You Feel?</div>
+            <div className='good-answer'>
+                Answer:<br />{guess}
+                <img draggable={false} className='audio-icon' src={audioIconGreen} alt='audio-icon' />
+            </div>
             <div className='user-feedback-bar'>
                 <div className='user-feedback good' onClick={() => handleGuess(guess, 1)}>
                     Feeling Great!
--- src/components/Incorrect.js
+++ src/components/Incorrect.js
@@ -1,12 +1,21 @@
+import audioIconGreen from '../images/audio-icon-green.png'
+import audioIconRed from '../images/audio-icon-red.png'
+
 const Incorrect = ({prev, guess, handleGuess}) => {
     return (
         <>
             <div className='bad-message'>Incorrect!</div>
             <div className='bad-answers'>
-                <div className='bad-answer selected'>Selected:<br />{guess}</div>
-                <div className='bad-answer answer'>Answer:<br />{guess}</div>
+                <div className='bad-answer selected'>
+                    Selected:<br />{guess}
+                    <img draggable={false} className='audio-icon' src={audioIconRed} alt='audio-icon' />
+                </div>
+                <div className='bad-answer answer'>
+                    Answer:<br />{prev}
+                    <img draggable={false} className='audio-icon' src={audioIconGreen} alt='audio-icon' />
+                </div>
             </div>
-            <h2 onClick={() => handleGuess(prev, 3, false, guess)}>Keep Practicing {'-->'}</h2>
+            <div className='bad-continue' onClick={() => handleGuess(prev, 3, false, guess)}>Keep Practicing {'-->'}</div>
         </>
     )
 }
