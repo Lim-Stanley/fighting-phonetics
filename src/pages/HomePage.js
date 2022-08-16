4cf3f3e6e70889b89e2f6bbb99e78c440aaab494
Stanley Lim <slim@Stanleys-MacBook-Air.local>
2022-08-16 11:07:54 -0400

    Homepage Added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,15 +8,20 @@ const HomePage = () => {
         <img draggable={false} className='gloves-icon' src={gloves} alt='logo' />
       </div>
       <div className='homepage-row two'>
-        <Link className='homepage-link' to="/english-consonants">To English Consonants</Link>
+        <Link className='homepage-link upper' to="/english-consonants">
+          <h1 className='homepage-symbol'>[ð]</h1> To English Consonants </Link>
         <h1 className="homepage-title one">fighting</h1>
         <h1 className="homepage-title two">phonetics</h1>
-        <Link className='homepage-link' to="/international-consonants">To International Consonants</Link>
+        <Link className='homepage-link upper' to="/international-consonants">
+          <h1 className='homepage-symbol'>[r]</h1>To International Consonants</Link>
       </div>
       <div className='homepage-row three'>
-        <Link className='homepage-link' to="/english-vowels">To English Vowels</Link>
-        <Link className='homepage-link' to="/non-pulmonic">To Non-Pulmonic Sounds</Link>
-        <Link className='homepage-link' to="/international-vowels">To International Vowels</Link>
+        <Link className='homepage-link' to="/english-vowels">
+          <h1 className='homepage-symbol'>[ə]</h1>To English Vowels</Link>
+        <Link className='homepage-link' to="/non-pulmonic">
+          <h1 className='homepage-symbol'>[ɠ]</h1>To Non-Pulmonic Sounds</Link>
+        <Link className='homepage-link' to="/international-vowels">
+          <h1 className='homepage-symbol'>[i]</h1>To International Vowels</Link>
       </div>
     </div>
   );
--- src/pages/IntCons.js
+++ src/pages/IntCons.js
@@ -53,7 +53,11 @@ const IntCons = () => {
 
     // Toggles a selected row on or off
     const toggle = (isRow, index) => {
-      if (!isRow) {
+      if (selected.rows.length == rows.length || selected.cols.length == cols.length){
+        if (isRow) { setSelected({rows: [index], cols: []})}
+        else {setSelected({rows: [], cols: [index * 2, index * 2 + 1]})}
+      }
+      else if (!isRow) {
         if (selected.cols.includes(index * 2)){
           setSelected({...selected, cols : selected.cols.filter(col => col != index * 2 && col != index * 2 + 1)})}
         else{ setSelected({...selected, cols : [...selected.cols, index * 2, index * 2 + 1]})}
